5ff1b347f4be09ad1f8d15f939864602b3fe2d62
NadiiaYurchenko <0679086844a@gmail.com>
2025-04-07 19:42:09 -0400

    Next commit

---
+++ venv/day01/concatination.py
@@ -0,0 +1,12 @@
+age = 20
+print("I am {} years old".format(age))
+
+prog_language = "python"
+version = 3.13
+
+print("My programming language is {} programming language and the version is {}".format(prog_language, version))
+
+print(f"My programming language is {prog_language} programming language and the version is {version}")
+
+# work only with string
+print("My programming language is " + prog_language)
---
+++ venv/day01/first_python_program.py
@@ -0,0 +1,17 @@
+print('Python Programing')
+
+#This is a comment
+
+
+
+name_of_student = "Elena"
+print(name_of_student)
+print(type(name_of_student))
+
+name_of_student = 3.5
+print(name_of_student)
+print(type(name_of_student))
+
+name_of_student = True
+print(name_of_student)
+print(type(name_of_student))
